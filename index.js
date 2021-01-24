f399e05b2191da9839d78f8ad6cb8e99e5c4f5b3
Dhruvil Dave <dhruvil.dave@outlook.com>
2021-01-24 18:04:50 +0530

    Added and tweaked a few configs

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
 
-app.use(express.static(__dirname + "/public/"));
+app.use(express.static(__dirname + '/public/'));
 app.use(
-    "/build/",
-    express.static(path.join(__dirname, "node_modules/three/build"))
+    '/build/',
+    express.static(path.join(__dirname, 'node_modules/three/build'))
 );
 app.use(
-    "/jsm/",
-    express.static(path.join(__dirname, "node_modules/three/examples/jsm"))
+    '/jsm/',
+    express.static(path.join(__dirname, 'node_modules/three/examples/jsm'))
 );
 
-app.listen(3000, () => console.log("Visit http://127.0.0.1:3000"));
+app.listen(3000, () => console.log('Visit http://127.0.0.1:3000'));
